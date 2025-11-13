191ba1c5ae74b45913da497087e9b33a087cde1d
phan-phuong-an <phanthiphuongan1412@gmail.com>
2025-11-13 17:17:10 +0700

    update css

--- src/data/sampleStudents.js
+++ src/data/sampleStudents.js
@@ -6,17 +6,76 @@ export const sampleStudents = [
     name: 'Anna',
     classCode: 'CKTGT1',
     status: 'Đã nhập học',
-    time: '2020-10-08',
+    time: '2025-10-09',
     note: ''
   },
+
   {
     id: 2,
     semester: '20201',
     studentId: '654662',
     name: 'Bob',
-    classCode: 'CKTGT1',
-    status: 'Chưa nhập học',
-    time: '2020-10-09',
+    classCode: 'CKTGT2',
+    status: 'Đã nhập học',
+    time: '2025-10-09',
     note: 'Sinh viên chuyển từ trường khác'
-  }
+  },
+
+   {
+    id: 3,
+    semester: '20201',
+    studentId: '815843',
+    name: 'Han',
+    classCode: 'CKTGT16',
+    status: 'Đã nhập học',
+    time: '2020-10-09',
+    note: ''
+  },
+  
+   {
+    id: 4,
+    semester: '20201',
+    studentId: '256482',
+    name: 'Nami',
+    classCode: 'CKTGT14',
+    status: 'Chưa nhập học',
+    time: '2025-10-09',
+    note: ''
+  },
+  
+   {
+    id: 5,
+    semester: '20201',
+    studentId: '654662',
+    name: 'Bo',
+    classCode: 'CKTGT2',
+    status: 'Đã nhập học',
+    time: '2025-10-09',
+    note: ''
+  },
+  
+   {
+    id: 6,
+    semester: '20201',
+    studentId: '624831',
+    name: 'Bob',
+    classCode: 'CKTGT1',
+    status: 'Đã nhập học',
+    time: '2025-10-09',
+    note: ''
+  },
+  
+   {
+    id: 7,
+    semester: '20201',
+    studentId: '146315',
+    name: 'Bobi',
+    classCode: 'CKTGT2',
+    status: 'Đã nhập học',
+    time: '2025-10-09',
+    note: ''
+  },
+
+  
+
 ];
